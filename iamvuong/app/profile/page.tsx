fc88298aff409ae20fe93dc97b79d584aacc17b8
vuongbui97 <vuongdevtest@gmail.com>
2025-04-30 11:04:26 +0700

    Update

---
+++ iamvuong/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="flex flex-col ">
+      <div>Name: Bui Tan Vuong</div>
+      <div>Age: Hidden...</div>
+      <div>Hobbies: Footballs, Games, Code</div>
+    </div>
+  );
+};
+
+export default Profile;
